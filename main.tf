76aad7e497278e6466bcea7b6b9ac365a62ac5f3
abhishek9234 <67266380+abhishek9234@users.noreply.github.com>
2024-12-22 00:09:12 +0530

    remove rg creation

--- main.tf
+++ main.tf
@@ -4,18 +4,11 @@ provider "azurerm" {
   subscription_id = "4c37f0e1-134e-4a4e-8d0f-fde9cb825586"
 }
 
-
-# Create a Resource Group
-resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
 # Create a Storage Account
 resource "azurerm_storage_account" "example" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
